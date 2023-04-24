bd518c18115a0df58d25b817b770385ae2d87313
younus <younusshaikh0313@gmail.com>
2023-04-24 23:05:48 +0530

    Array Project

---
+++ Ecommerce/src/com/GitPractice/SortArrayAsending.java
@@ -0,0 +1,13 @@
+package com.GitPractice;
+
+import java.util.Arrays;
+
+public class SortArrayAsending {
+	public static void main(String[] args) {
+		int a[] = { 10, 20, 55, 23, 62, 98, 52, 47 };
+
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+	}
+
+}
