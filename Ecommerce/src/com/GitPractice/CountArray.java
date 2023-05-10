aca499b138126017add44696f2420f52936fd7e8
younus <younusshaikh0313@gmail.com>
2023-05-10 14:10:58 +0530

    Array Count

---
+++ Ecommerce/src/com/GitPractice/CountArray.java
@@ -0,0 +1,13 @@
+package com.GitPractice;
+
+public class CountArray {
+	public static void main(String[] args) {
+		int a[] = { 10, 20, 30, 65, 89, 54, 74 };
+		for (int i = 0; i < a.length; i++) {
+
+		}
+		// Counting the Length Of Array
+		System.out.println(a.length);
+	}
+
+}
